e01f9dcabc5b76a4026165ae0ec2a761b4c2bd63
Imam Faried <35225373+imamfh24@users.noreply.github.com>
2023-01-25 03:53:35 +0700

    Initialize AmryRP

---
+++ Assets/Scripts/Interfaces/InterfaceCharacter.cs
@@ -0,0 +1,83 @@
+<<<<<<< Updated upstream
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//game over when energy zero
+public interface ISinkable
+{
+    void Kill();
+}
+
+//get hit by obstacle
+public interface IDrainable<T>
+{
+    void Drain(T damageTaken);
+}
+
+//change equip
+public interface IChangeable<T>
+{
+    void Change(T Type);
+}
+
+public interface IMoveable<T>
+{
+    IEnumerator MovePlayer(T moveSpeed);
+    void WaterStreamFlow(T Flow);
+}
+public interface IStopable<T>
+{
+    IEnumerator BreakPlayer(T breakValue);
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+
+//game over when energy zero
+public interface ISinkable
+{
+    void Kill();
+    void Drain();
+}
+
+//get hit by obstacle
+public interface IDrainable<T>
+{
+    void Damage(T damageTaken);
+}
+
+//change equip
+public interface IChangeable<T>
+{
+    void Change(T Type);
+}
+
+public interface IMoveable<T>
+{
+    IEnumerator MovePlayer(T moveSpeed);
+
+    void DistanceTravel();
+}
+public interface IStopable<T>
+{
+    IEnumerator BreakPlayer(T breakValue);
+}
+[System.Serializable]
+public class SerializableDictionary<TK, TV>: ISerializable
+{
+    private Dictionary<TK, TV> _Dictionary;
+    [SerializeField] List<TK> _Keys;
+    [SerializeField] List<TV> _Values;
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+>>>>>>> Stashed changes
+}
